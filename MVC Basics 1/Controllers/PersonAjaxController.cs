fda21ce738c328594ea36b4b1331104dc6c5cdc6
sitiekram <ekramseid4@gmail.com>
2021-11-20 12:52:08 +0100

    inital

---
+++ MVC Basics 1/Controllers/PersonAjaxController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Basics_1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Basics_1.Controllers
+{
+    public class PersonAjaxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetPeople()
+        {
+            PeopleViewModel peopleView = new PeopleViewModel();
+            peopleView.PersonList = peopleView.Read();
+            if (peopleView.PersonList.Count == 0 || peopleView.PersonList == null)
+            {
+                peopleView.SeedPeople();
+            }
+            List<Person> personList = peopleView.Read();
+            return PartialView("_AjaxPersonPartial", personList);
+        }
+        [HttpPost]
+        public IActionResult FindPersonById(int personID)
+        {
+            PeopleViewModel peopleView = new PeopleViewModel();
+            Person targetPerson = peopleView.Read(personID);
+            List<Person> personList = new List<Person>();
+            if (targetPerson != null)
+            {
+                personList.Add(targetPerson);
+            }
+            return PartialView("_AjaxPersonPartial", personList);
+        }
+        [HttpPost]
+        public IActionResult DeletePersonById(int personID)
+        {
+            PeopleViewModel peopleView = new PeopleViewModel();
+            bool status = false;
+            Person targetPerson = peopleView.Read(personID);
+            if (targetPerson != null)
+            {
+                status = peopleView.Delete(targetPerson);
+            }
+            if (status)
+            {
+                return StatusCode(200);
+            }
+            return StatusCode(404);
+        }
+    }
+}
---
+++ MVC Basics 1/wwwroot/js/PersonScript.js
@@ -0,0 +1,24 @@
+﻿function getAllPeople() {
+    $.get("/PersonAjax/GetPeople", null, function (data) {
+        $("#PersonList").html(data);
+    });
+}
+function getPersonByID() {
+    var personIDValue = document.getElementById('PersonIdInput').value;
+    $.post("/PersonAjax/FindPersonById", { personID: personIDValue }, function (data) {
+        $("#PersonList").html(data);
+    });
+}
+function deletePersonByID() {
+    var personIDValue = document.getElementById("PersonIdInput").value;
+    $.post("/PersonAjax/DeletePersonById", { personID: personIDValue }, function (data) {
+
+    })
+        .done(function () {
+            document.getElementById('errorMessages').innerHTML = "Successfully Deleted person.";
+            getAllPeople();
+        })
+        .fail(function () {
+            document.getElementById('errorMessages').innerHTML = "Could not delete person.";
+        });
+}
