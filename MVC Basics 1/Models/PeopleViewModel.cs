41d7d8e90ca68ba6b2556cc72c5f199920734582
sitiekram <ekramseid4@gmail.com>
2021-11-25 11:54:02 +0100

    Model update

--- MVC Basics 1/Models/PeopleViewModel.cs
+++ MVC Basics 1/Models/PeopleViewModel.cs
@@ -7,76 +7,11 @@ namespace MVC_Basics_1.Models
 {
     public class PeopleViewModel : CreatePersonViewModel
     {
-        private static List<Person> personList = new List<Person>();
-        private static int _idCounter;
         public List<Person> PersonList { get; set; }
         public string FilterString { get; set; }
-        public void SeedPeople()
+        public PeopleViewModel()
         {
-            PeopleViewModel peopleView = new PeopleViewModel();
-            peopleView.Create("Ekram Ahmedin", "Trollhättan", "0723233234", "esdf3@gmail.com");
-            peopleView.Create("Eve Ulf", "Göteborg", "0734567453", "wsdfgjjgv@gmail.com");
-            peopleView.Create("Marwan Ali", "Vänersborg", "0789435634", "s4fdg@hotmail.com");
-            peopleView.Create("Andersson Tomas", "Göteborg", "+46792055765", "rahwaali2@gmail.com");
-            peopleView.Create("Sofia Abdullah", "Vänersborg", "+46792511963", "bubt78s@hotmail.com");
-            peopleView.Create("Rahwa Suliman", "Stockholm", "0793498567", "reregs5@gmail.com");
+            PersonList = new List<Person>();
         }
-        public Person Create(string fullName, string city, string phoneNumber, string email)
-        {
-            Person newPerson = new Person(_idCounter, fullName, city, phoneNumber, email);
-            _idCounter++;
-            personList.Add(newPerson);
-            return newPerson;
-        }
-        public bool Delete(Person person)
-        {
-            bool status = personList.Remove(person);
-            return status;
-        }
-        public List<Person> Read()
-        {
-            return personList;
-        }
-
-        public Person Read(int id)
-        {
-            Person targetPerson = personList.Find(p => p.PersonId == id);
-            return targetPerson;
-        }
-        /*
-         private static List<Person> personList = new List<Person>();
-        private static int _idCounter;
-        public void SeedPeople()
-        {
-            PeopleMemory peopleMemory = new PeopleMemory();
-            peopleMemory.Create("Ekram Ahmedin", "Trollhättan", "0723233234", "esdf3@gmail.com");
-            peopleMemory.Create("Eve Ulf", "Göteborg", "0734567453", "wsdfgjjgv@gmail.com");
-            peopleMemory.Create("Marwan Ali", "Vänersborg", "0789435634", "s4fdg@hotmail.com");
-            peopleMemory.Create("Andersson Tomas", "Göteborg", "+46792055765", "rahwaali2@gmail.com");
-            peopleMemory.Create("Sofia Abdullah", "Vänersborg", "+46792511963", "bubt78s@hotmail.com");
-            peopleMemory.Create("Rahwa Suliman", "Stockholm", "0793498567", "reregs5@gmail.com");
-        }
-        public Person Create(string fullName, string city, string phoneNumber, string email)
-        {
-            Person newPerson = new Person(_idCounter, fullName, city, phoneNumber, email);
-            _idCounter++;
-            personList.Add(newPerson);
-            return newPerson;
-        }
-        public bool Delete(Person person)
-        {
-            bool status = personList.Remove(person);
-            return status;
-        }
-        public List<Person> Read()
-        {
-            return personList;
-        }
-
-        public Person Read(int id)
-        {
-            Person targetPerson = personList.Find(p => p.PersonId == id);
-            return targetPerson;
-        }*/
     }
 }
